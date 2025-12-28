5282b742097a9813446518bc7a5f797be80f316e
hashhunter-1312 <i221644@nu.edu.pk>
2025-12-28 20:31:18 +0500

    Add simple Java app with JUnit test

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,11 @@
+package main.java;
+
+public class HelloWorld {
+    public static String sayHello() {
+        return "Hello, Jenkins!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sayHello());
+    }
+}
